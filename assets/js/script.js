991fea9a82809bf560baeb7eaa7c6871b2010e57
Ermon Kyle Aradanas Antonio <8772405+ermon07@users.noreply.github.com>
2026-07-22 13:28:44 +0800

    Test

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,9 +1,33 @@
-const navbar = document.querySelector('.museum-navbar');
+const navbar = document.querySelector(".museum-navbar");
 
-window.addEventListener('scroll', () => {
+window.addEventListener("scroll", () => {
   if (window.scrollY > 40) {
-    navbar.classList.add('scrolled');
+    navbar.classList.add("scrolled");
   } else {
-    navbar.classList.remove('scrolled');
+    navbar.classList.remove("scrolled");
+  }
+});
+
+const themeToggle = document.getElementById("theme-toggle");
+
+const body = document.body;
+
+if (localStorage.getItem("theme") === "dark") {
+  body.classList.add("dark-mode");
+
+  themeToggle.innerHTML = '<i class="bi bi-sun-fill"></i>';
+}
+
+themeToggle.addEventListener("click", () => {
+  body.classList.toggle("dark-mode");
+
+  if (body.classList.contains("dark-mode")) {
+    localStorage.setItem("theme", "dark");
+
+    themeToggle.innerHTML = '<i class="bi bi-sun-fill"></i>';
+  } else {
+    localStorage.setItem("theme", "light");
+
+    themeToggle.innerHTML = '<i class="bi bi-moon-fill"></i>';
   }
 });
