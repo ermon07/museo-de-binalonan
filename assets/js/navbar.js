2dd1b3d6252ff645bad003f4816af1d087195b19
Ermon Kyle Aradanas Antonio <8772405+ermon07@users.noreply.github.com>
2026-07-27 13:29:54 +0800

    test

---
+++ assets/js/navbar.js
@@ -0,0 +1,15 @@
+const navbar = document.querySelector(".museum-navbar");
+
+
+if(navbar){
+
+    window.addEventListener("scroll",()=>{
+
+        navbar.classList.toggle(
+            "scrolled",
+            window.scrollY > 40
+        );
+
+    });
+
+}
