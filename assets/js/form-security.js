3b89b6b1ad67bffdbe544f6f444b1c57e8e73236
Ermon Kyle Aradanas Antonio <8772405+ermon07@users.noreply.github.com>
2026-07-28 15:17:53 +0800

    test

---
+++ assets/js/form-security.js
@@ -0,0 +1,61 @@
+document.addEventListener(
+"DOMContentLoaded",
+()=>{
+
+
+const forms=document.querySelectorAll(
+".visit-contact-form"
+);
+
+
+
+forms.forEach(form=>{
+
+
+form.addEventListener(
+"submit",
+(e)=>{
+
+
+const trap=form.querySelector(
+"name='website'"
+);
+
+
+
+if(trap.value !== ""){
+
+    e.preventDefault();
+
+    return false;
+
+}
+
+
+
+const message=form.querySelector(
+"textarea[name='message']"
+);
+
+
+
+if(message.value.length > 500){
+
+    e.preventDefault();
+
+    alert(
+    "Message is too long."
+    );
+
+    return false;
+
+}
+
+
+});
+
+
+});
+
+
+});
